40dd43ff551523db1c3f5d70c7900f581e7bbaa8
kimjieon0 <keonj18@gmail.com>
2021-07-15 15:37:43 +0900

    hi

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Hello world!');
+    res.send('hi!');
 });
 
 app.get('/start', function(req, res){
